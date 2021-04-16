55e039c0a059594cf93a55c358a9cae0e5d9dfad
Eduardo Coelho <38166111+Eduardo-Coelho@users.noreply.github.com>
2021-04-16 13:26:40 +0100

    Refactoring the moke file

--- node/server.js
+++ node/server.js
@@ -3,9 +3,16 @@ const app = express();
 
 app.listen(1234, () => {
   app.get("/Home", (req, res, next) => {
-    console.log("Home", req);
-    const file = require(`./Data/mock.json`);
+    const file = require(`./Data/Home/FeaturedArt.json`);
     return res.json(file);
   });
+  app.get("/Product", (req, res, next) => {
+    if (req.params) {
+      const file = require(`./Data/Products/${req.params}.json`);
+      return res.json(file);
+    }
+    console.log("error with the /Product params ->", req.params);
+  });
+
   console.log("Server running on port 1234");
 });
