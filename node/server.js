2f04ddef0de80616d65cb5845aeaf78bace79b96
Eduardo Coelho <38166111+Eduardo-Coelho@users.noreply.github.com>
2021-04-18 12:48:38 +0100

    refactoring node server

--- node/server.js
+++ node/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.listen(1234, () => {
-  app.get("/Home", (req, res, next) => {
-    const file = require(`./Data/Home/FeaturedArt.json`);
+app.get("/home", (req, res, next) => {
+  const file = require(`./data/home/FeaturedArt.json`);
+  return res.json(file);
+});
+app.get("/product", (req, res, next) => {
+  try {
+    const { productType } = req.query;
+    console.log("params->", productType);
+    const file = require(`./data/products/${productType}/${productType}.json`);
     return res.json(file);
-  });
-  app.get("/Product", (req, res, next) => {
-    try {
-      const { productType } = req.query;
-      console.log("params->", productType);
-      const file = require(`./Data/Products/${productType}/${productType}.json`);
-      return res.json(file);
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+app.listen(1234, () => {
   console.log("Server running on port 1234");
 });
--- src/ENDPOINT_URL.ts
+++ src/ENDPOINT_URL.ts
@@ -1,6 +1,6 @@
 enum ENDPOINT_URL {
-    Home = 'http://localhost:1234/Home',
-    Product = 'http://localhost:1234/Product?productType=',
+    Home = 'http://localhost:1234/home',
+    Product = 'http://localhost:1234/product?productType=',
 }
 
 export default ENDPOINT_URL;
