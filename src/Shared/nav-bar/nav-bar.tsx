b679f83e5e8e71df3f9000e3eae4f54f004ca749
Eduardo Coelho <38166111+Eduardo-Coelho@users.noreply.github.com>
2021-04-22 18:58:24 +0100

    done some work to the navbar cass/html , to make it look a bit better

--- src/Shared/nav-bar/nav-bar.tsx
+++ src/Shared/nav-bar/nav-bar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./nav-bar.scss";
-import Grid from "@material-ui/core/Grid";
 import { Link } from "react-router-dom";
 
 const NavBar: React.FC = () => {
@@ -11,34 +10,21 @@ const NavBar: React.FC = () => {
           <div className="nav-logo">
             <h1>Crypto Art Shop</h1>
           </div>
-          <div className="nav-controls">
-            <div className="nav-root">
-              <Grid
-                container
-                direction="row"
-                justify="flex-end"
-                alignItems="flex-end"
-              >
-                <Grid item xs={1}>
-                  <div className="nav-item">
-                    <Link to={{ pathname: "/" }}>
-                      <p>Home</p>
-                    </Link>
-                  </div>
-                </Grid>
-                <Grid item xs={2}>
-                  <div className="nav-item">
-                    <p>How Cryptoart Works</p>
-                  </div>
-                </Grid>
-                <Grid item xs={1}>
-                  <div className="nav-item">
-                    <p>Gallery</p>
-                  </div>
-                </Grid>
-              </Grid>
-            </div>
-          </div>
+          <ul className="nav-controls">
+            <li className="nav-item">
+              <p>How Cryptoart Works</p>
+            </li>
+
+            <li className="nav-item">
+              <p>Gallery</p>
+            </li>
+
+            <li className="nav-item">
+              <Link to={{ pathname: "/" }}>
+                <p>Home</p>
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
